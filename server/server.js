6d3eaececbc0f5ee2b4fcd055f903c5f542e4793
Vemana Bhavani Shankar <109222997+Vb-Shankar@users.noreply.github.com>
2024-08-07 02:19:04 +0530

    removed localhost

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 const cors = require('cors');
 
 mongoose.connect('mongodb+srv://vemanavbs2139:uqrfVdvTjNcslC9H@cluster0.iehxdto.mongodb.net/');
